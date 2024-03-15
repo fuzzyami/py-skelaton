a83429f8e8e1ecb2d8c4e0e135ae651bc397d9a6
Ami  Blonder <ami@cycognito.com>
2024-03-15 23:46:11 +0200

    add postgres test endpoints

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 import flask
-import psycopg2
+from src.postgres_utils import setup_pg_db, read_from_pg_db, write_to_pg_db
 import requests
 import pymongo
 import pika
 from src.redis_utils import read_key_from_redis, write_key_to_redis
+from flask import jsonify
 
 # create flask app
 app = flask.Flask(__name__)
@@ -21,8 +22,19 @@ def redis_write_test(key, value):
 def redis_read_test(key):
     return read_key_from_redis(key)
 
+@app.route("/postgres/read")
+def postgres_read_test():
+    results  = read_from_pg_db()
+    return jsonify(results)
+
+@app.route("/postgres/write/<user_id>/<user_name>")
+def postgres_write_test(user_id, user_name):
+    write_to_pg_db(user_id,user_name)
+    resp = jsonify(success=True)
+    return resp
 
 def main():
+    setup_pg_db()
     app.run(host='localhost', port=8082)
 
 if __name__ == '__main__':
